1990e479c946c1b4e4e94a16f72d428f354d3a5a
eunbin-jang <eunbin-jang@froide.co.jp>
2019-12-09 18:20:54 +0900

    ランダムの今日すること

---
+++ randomman.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class randomman {
+  public static void main(String[] args) {
+    
+    String name; 
+    System.out.println("あなたの名前を入力してください");
+    Scanner scanner = new Scanner(System.in);
+    name = scanner.nextLine(); 
+
+    dousa();
+
+    // System.out.println("今日、" + name + "さんは" + get_Flag + "をします。");
+  
+}
+  
+  public static void dousa() {
+  Random random = new Random();
+    System.out.println("「今日あなたがすること」");
+    int get_Flag = random.nextInt(3);
+      
+      switch (get_Flag) {
+        case 0:
+          System.out.println("食事");
+          break;
+        case 1:
+          System.out.println("運動");
+          break;
+        case 2:
+          System.out.println("睡眠");
+        case 3:
+          System.out.println("ゲーム");
+          break;
+        default:
+          System.out.println("勉強");
+        break;
+    }
+
+    System.out.println("今日、" + name + "さんは" + get_Flag + "をします。");
+    
+  }
+}
