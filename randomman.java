0d39746c2e6df65b38fd8c07522710fee92c9afe
eunbin-jang <eunbin-jang@froide.co.jp>
2019-12-10 10:58:44 +0900

    ランダムで決める今日すること

--- randomman.java
+++ randomman.java
@@ -3,38 +3,42 @@ import java.util.Random;
 
 public class randomman {
   public static void main(String[] args) {
-    
-    String name; 
+    String name;
+
     System.out.println("あなたの名前を入力してください");
     Scanner scanner = new Scanner(System.in);
     name = scanner.nextLine(); 
 
-    dousa();
+    String Ugoki = dousa();
 
-    // System.out.println("今日、" + name + "さんは" + get_Flag + "をします。");
-    //　ここむずい
+    System.out.println("今日、" + name + "さんは" + Ugoki + "をします。");
 }
   
-  public static void dousa() {
-  Random random = new Random();
-    System.out.println("「今日あなたがすること」");
-    int get_Flag = random.nextInt(3);
-      
-      switch (get_Flag) {
-        case 0:
-          System.out.println("食事");
+
+  public static String dousa() {
+    String Ugoki = new String();
+
+    Random random = new Random();
+      System.out.println("「今日あなたがすること」");
+      int get_Flag = random.nextInt(3);
+
+        switch (get_Flag) {
+          case 0:
+            Ugoki = "食事";
+            break;
+          case 1:
+            Ugoki = "運動";
+            break;
+          case 2:
+            Ugoki = "睡眠補充";
+            break;
+          case 3:
+            Ugoki = "ゲーム";
+            break;
+          default:
+            Ugoki = "勉強";
           break;
-        case 1:
-          System.out.println("運動");
-          break;
-        case 2:
-          System.out.println("睡眠");
-        case 3:
-          System.out.println("ゲーム");
-          break;
-        default:
-          System.out.println("勉強");
-        break;
-    }
+        }
+      return Ugoki;
   }
 }
