7960543882ed3416354c1b28e9f169b998f20290
eunbin-jang <eunbin-jang@froide.co.jp>
2019-12-11 09:41:46 +0900

    名前入力追加

--- Age.java
+++ Age.java
@@ -4,13 +4,17 @@ import java.util.Scanner;
 public class Age {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String name;
+
+        System.out.println("あなたの名前を入力してください");
+        name = scanner.nextLine();
       
         System.out.println("あなたの年齢を入力してください");
             while(true) {
                 int yourAge = scanner.nextInt();
                 yourAge++;
                 
-                System.out.println("2020年のあなたは" + yourAge + "歳ですね。");
+                System.out.println("2020年の" + name + "さんは" + yourAge + "歳ですね。");
                 break;
             }
         }
